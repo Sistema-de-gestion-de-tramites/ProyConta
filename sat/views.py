4f17d1ce5365a58a31a8621d0b4b77de5635efe6
Gerardo3d <josegera60@gmail.com>
2022-10-23 19:40:15 -0500

    Prueba inicial

--- sat/views.py
+++ sat/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def prueba(request):
+    return render(request,'prueba/base.html')
+
--- siteconta/settings.py
+++ siteconta/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Mis apps
+    'sat',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'siteconta.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
