d505645d1c3743e80001aa9a6173e9a90a015708
EricIbanezH <erickibanezhernandezxd@hotmail.com>
2023-04-28 13:32:03 -0600

    adicion del archivo datos_iniciales.sql para configurar permisos

---
+++ datos_inciales.sql
@@ -0,0 +1,5 @@
+select * from empleados;
+insert into empleados(nombre_completo) values('Coordinadora ISC');
+
+insert into alumnos(nombre_completo,no_control) values('Pablo Lopez','19140332');
+insert into alumnos(nombre_completo,no_control) values('Juan perez','19140335');
