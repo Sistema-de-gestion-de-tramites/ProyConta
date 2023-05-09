08f511f4fdbef985e9c6db8d75a676081063f8d4
EricIbanezH <erickibanezhernandezxd@hotmail.com>
2023-05-08 22:35:52 -0600

    solucion errores despues de hacer merge front2, mostrar username de la sesion y su rol (primero en su lista de roles)

--- siteconta/settings.py
+++ siteconta/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         },
         'NAME': 'project_ing',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'oracle',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
@@ -122,7 +122,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-mx'
 
 TIME_ZONE = 'UTC'
 
